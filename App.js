bb40417c62d42035ec76e57759fef41575a885c4
Andrew Fuller <andrew.fuller@gemini.com>
2020-11-09 23:35:46 -0500

    Check async and note that reloadAsync does not load new files

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import * as Updates from "expo-updates";
+import { Alert, Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>
+        Using Embeddeded Assets: {String(Updates.isUsingEmbeddedAssets)}
+      </Text>
+      <Text>manifest: {JSON.stringify(Updates.manifest?.commitTime)}</Text>
+
+      <Text>Test</Text>
+
+      <Button
+        title="Press to reload"
+        onPress={async () => {
+          const check = await Updates.checkForUpdateAsync();
+          if (check.isAvailable) {
+            await Updates.fetchUpdateAsync();
+            await Updates.reloadAsync();
+          }
+        }}
+      />
     </View>
   );
 }
@@ -14,8 +30,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
